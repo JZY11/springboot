5f1fadd38002135c4724993da36092cddf4762fa
jzy <jiangzy1@lenovo.com>
2018-08-22 10:49:56 +0800

    springboot demo

---
+++ java/cn/itcast/springboot/javaconfig/User.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package cn.itcast.springboot.javaconfig;
+
+/**
+ * @ClassName User
+ * @Description
+ * @Author jzy
+ * @Date 2018年8月22日
+ */
+public class User {
+
+	private String username;
+
+    private String password;	
+
+    private Integer age;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
